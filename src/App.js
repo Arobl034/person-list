9027edf164dc122c95e8bc1c26a948809c09d501
Arobl034 <robles305@gmail.com>
2020-10-18 15:00:35 -0400

    person list initialized

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => <PersonList />;
+
+const PersonList = () => {
+  const people = [
+    {
+      img: 22,
+      name: "yemil",
+      job: "RE1",
+    },
+    {
+      img: 19,
+      name: "rob",
+      job: "RE1",
+    },
+    {
+      img: 22,
+      name: "anthony",
+      job: "RE2",
+    },
+    {
+      img: 25,
+      name: "keith",
+      job: "RE2",
+    },
+    {
+      img: 14,
+      name: "maria",
+      job: "RE2",
+    },
+    {
+      img: 8,
+      name: "jessy",
+      job: "RE2",
+    },
+    {
+      img: 20,
+      name: "serio",
+      job: "Specialist",
+    },
+    {
+      img: 15,
+      name: "michael",
+      job: "Supervisor",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section>
+      <Person person={people[7]}/>
+      <Person person={people[6]}/>
+      <Person person={people[5]}/>
+      <Person person={people[4]}/>
+      <Person person={people[3]}/>
+      <Person person={people[2]}/>
+      <Person person={people[1]}/>
+      <Person person={people[0]}/>
+    </section>
+  );
+};
+
+const Person = (props) => {
+  const {img,name,job} = props.person;
+  const {children} = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return (
+    <div className="person">
+      <img src={url} alt="" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
