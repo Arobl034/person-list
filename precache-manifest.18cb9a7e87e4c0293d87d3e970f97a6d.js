259fb8a769ffab06dcc05a5c247ec93c03a2c47f
Arobl034 <robles305@gmail.com>
2020-10-18 15:11:01 -0400

    Updates

---
+++ precache-manifest.18cb9a7e87e4c0293d87d3e970f97a6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f849774e07a012a44e72a09bb5dab6",
+    "url": "/person-list/index.html"
+  },
+  {
+    "revision": "91a32ceebf82b602f750",
+    "url": "/person-list/static/css/main.1417d8e1.chunk.css"
+  },
+  {
+    "revision": "4883f354e8e9f08fa048",
+    "url": "/person-list/static/js/2.6522d073.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/person-list/static/js/2.6522d073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a32ceebf82b602f750",
+    "url": "/person-list/static/js/main.323f5555.chunk.js"
+  },
+  {
+    "revision": "7d9cebbf61eb7307c32a",
+    "url": "/person-list/static/js/runtime-main.836e0ac7.js"
+  }
+]);
